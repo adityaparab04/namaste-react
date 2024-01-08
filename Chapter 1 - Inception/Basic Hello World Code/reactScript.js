4f2a8eedc55ac686a360d5a47a5d3fd084b655d1
adityaparab04 <adityaparab04@gmail.com>
2024-01-08 13:07:04 -0500

    Chapter 1 completed

--- Chapter 1 - Inception/Basic Hello World Code/reactScript.js
+++ Chapter 1 - Inception/Basic Hello World Code/reactScript.js
@@ -1,8 +1,28 @@
-const heading = React.createElement("h1", {}, "Hello World from React"); 
-//createElement is React method and takes 3 arguments - element(tag) name, empty object and the innerHTML(i.e. content)
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React"
+);
+//createElement is React method and takes 3 arguments - react element(tag) name, empty object and the innerHTML(i.e. content)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //createRoot is a ReactDOM method and usually is in the index.html base file
 
 root.render(heading);
 //rendering the root with our heading
+
+//creating a nested parent-child react element
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading1" }, "I am, an H1 tag!"),
+    React.createElement("h2", { id: "heading2" }, "I am, an H2 tag!"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading1" }, "I am, an H1 tag!"),
+    React.createElement("h2", { id: "heading2" }, "I am, an H2 tag!"),
+  ]),
+]);
+
+console.log(parent);
+
+root.render(parent);
